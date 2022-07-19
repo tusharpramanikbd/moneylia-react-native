adb1c23380f752de83205efcd88c12aeae2f8d72
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-07-19 13:25:46 +0600

    login screen design implementation done

--- App.tsx
+++ App.tsx
@@ -3,18 +3,5 @@ import { StyleSheet, Text, View } from 'react-native'
 import LoginScreen from './screens/LoginScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreen />
-    </View>
-  )
+  return <LoginScreen />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('../assets/images/login-image.png')}
+      />
+      <Text style={styles.titleText}>Save your money</Text>
+      <Text style={styles.loremText}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est in quis
+        erat a sit.
+      </Text>
+      <View style={styles.buttonStyle}>
+        <Pressable android_ripple={{ color: '#dddddd' }}>
+          <Text style={styles.buttonText}>Join for free</Text>
+        </Pressable>
+      </View>
+      <Text style={styles.linkText}>Don't have SPID or CIE? Find out more</Text>
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 40,
+  },
+  image: {
+    width: 335,
+    height: 334,
+    marginBottom: 24,
+  },
+  titleText: {
+    textAlign: 'center',
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#3A3B7B',
+    marginBottom: 8,
+  },
+  loremText: {
+    textAlign: 'center',
+    marginBottom: 60,
+    color: '#6F6D7B',
+  },
+  buttonStyle: {
+    marginBottom: 24,
+    width: '85%',
+    backgroundColor: '#7476ED',
+    borderRadius: 4,
+    elevation: 4,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    paddingVertical: 16,
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  linkText: {
+    color: '#3A3B7B',
+    textDecorationLine: 'underline',
+  },
+})
